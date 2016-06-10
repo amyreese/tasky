2a56a88813a75eb0420659e727d2ab3d96a9dca5
John Reese <john@noswap.com>
2016-06-10 00:12:24 -0700

    Improve tasky imports

--- tasky/__init__.py
+++ tasky/__init__.py
@@ -2,8 +2,11 @@
 # Licensed under the MIT license
 # flake8: noqa
 
+from concurrent.futures import CancelledError
+
 from .tasks import Task, PeriodicTask, TimerTask, QueueTask
 from .config import Config, JsonConfig
+from .stats import Stats
 from .loop import Tasky
 
 __version__ = '0.6.2'
